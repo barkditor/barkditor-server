1103e293a769959c147a8e762a0e1d6c39597c46
Khachatur Khachatryan <xachulxx@gmail.com>
2023-01-08 16:03:22 +0300

    bugfix: fixed file path

--- BarkditorServer.Domain/Constants/FilePaths.cs
+++ BarkditorServer.Domain/Constants/FilePaths.cs
@@ -2,6 +2,6 @@ namespace BarkditorServer.Domain.Constants;
 
 public static class FilePaths
 {
-    private const string PathToBusinessLogic = "../../../../BarkdtiorServer.BusinessLogic/bin/Debug/net6.0/";
+    private const string PathToBusinessLogic = "../../../../BarkditorServer.BusinessLogic/bin/Debug/net6.0/";
     public const string ProjectFilesTreeJsonPath = $"{PathToBusinessLogic}projectFilesTree.json";
 }
